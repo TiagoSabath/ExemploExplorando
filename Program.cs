827412bd990b7a9b061e57c4ed1fa8dbf2a786df
Tiago Sabath <tiago.sabath@gmail.com>
2023-11-01 09:59:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using ExemploExplorando.Models;
+
+Pessoa P1 = new Pessoa(nome: "Tiago", sobrenome: "Sabath");
+
+Pessoa P2 = new Pessoa();
+P2.Nome = "felipe";
+P2.Sobrenome = "Sousa";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(P1);
+cursoDeIngles.AdicionarAluno(P2);
+cursoDeIngles.ListarAlunos();
